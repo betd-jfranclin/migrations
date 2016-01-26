f2de8ea4bb9b9d1319eddf2dbbc589116451fc3f
jfranclin <jfranclin.bd@gmail.com>
2016-01-26 10:57:24 +0100

    Adding git ignore

--- migrations-db.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
-return array(
-        'dbname'   => 'psa-dspp',
-        'user'     => 'root',
-        'password' => 'root',
-        'host'     => '192.168.10.11',
-        'driver'   => 'pdo_mysql'
-);
